b74021c32ee32201d3312db4e44af496099cadec
唐道源 <tangdaoyuan@megvii.com>
2022-07-13 14:30:58 +0800

    feat: get drivelist

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-export default '@suger-tdy/drivelist-all'
+import os from 'os'
+import { list as wList } from '@suger-tdy/drivelist-windows'
+import { list as oList } from '@suger-tdy/drivelist-osx'
+import { list as lList } from '@suger-tdy/drivelist-linux'
+
+export async function list() {
+  const plat = os.platform()
+
+  if (plat === 'linux')
+    return await lList()
+
+  else if (plat === 'win32')
+    return await wList()
+
+  else if (plat === 'darwin')
+    return await oList()
+
+  throw new Error(`Unsupported platform: ${plat}`)
+}
