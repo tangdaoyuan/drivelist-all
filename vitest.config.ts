48f5742fbd52237c783bfc1a790efffb0983ad71
唐道源 <tangdaoyuan@megvii.com>
2022-07-15 14:43:23 +0800

    chore: upgrade deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,8 +2,10 @@ import path from 'path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  alias: {
-    '@': path.resolve(__dirname, './src'),
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   test: {
     clearMocks: true,
